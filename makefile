ea82e49de33cc1b6c8e96e561ef4166c39161e29
Michael Linderman <mlinderman@middlebury.edu>
2026-01-01 13:39:33 -0500

    Simplify rendering, make all content visible in HTML output, but not in the live notebooks.

--- makefile
+++ makefile
@@ -5,30 +5,24 @@ SHELL = /bin/zsh
 PYTHON ?= env/bin/python3
 
 stage: 
-	$(PYTHON) scripts/prep-qmd.py
-	quarto render --profile publish
+	quarto render
 	$(PYTHON) scripts/create-ipynb.py
-	$(PYTHON) scripts/insert-colab-link.py
 
 publish: 
-	$(PYTHON) scripts/prep-qmd.py
-	quarto render --profile publish
+	quarto render
 	$(PYTHON) scripts/create-ipynb.py
-	$(PYTHON) scripts/insert-colab-link.py
 	git add .
 	git commit -m "Update"
 	git push
 
-prep: 
+prep:
 	$(PYTHON) scripts/create-ipynb.py
-	$(PYTHON) scripts/prep-qmd.py
 
 preview: 
-	quarto preview --profile preview
+	quarto preview
 
 clean: 
 	find . -type f -name "* [0-9]*" -delete
 	find . -name "* [0-9]*" -type d -exec rm -r "{}" \;
 	rm -rf docs	
-	rm -rf chapters
 
--- scripts/create-ipynb.py
+++ scripts/create-ipynb.py
@@ -1,40 +1,103 @@
+#!/usr/bin/env python3
+import argparse
+import contextlib
+import json
 import os
 import nbformat as nb
 import re
 import shutil
+import subprocess
+from dotenv import load_dotenv
+from pathlib import Path
 
+@contextlib.contextmanager
+def temporary_softlink(target_path, link_path):
+    """
+    A context manager to create a temporary softlink.
 
-os.makedirs("docs/live-notebooks", exist_ok=True)
-for f in os.listdir("source"):
-    if f.endswith(".qmd"):
-        out_path = f"docs/live-notebooks/{f.replace('qmd', 'ipynb')}"
-        os.system(f"quarto convert source/{f} -o {out_path}")
-        
-    elif f.endswith(".ipynb"):
-        
-        out_path = f"docs/live-notebooks/{f}"
-        shutil.copyfile(f"source/{f}", out_path)
-        
-    notebook = nb.read(out_path, as_version = 4)
+    Args:
+        target_path (str or Path): The destination the softlink should point to.
+        link_path (str or Path): The location where the softlink should be created.
+    """
+    try:
+        os.symlink(target_path, link_path)
+        yield link_path # Yield control to the 'with' block
+    finally:
+        # Removed link on exit
+        if os.path.islink(link_path):
+            os.remove(link_path)
+
+def create_notebook(src_path: Path, out_path: Path, site_url: str):
+    """Create notebook from .qmd or .ipynb file, rendering qmd to remove quarto-specific content.
+
+    Args:
+        src_path (Path): Path to the source file (.qmd or .ipynb)
+        out_path (Path): Path where the output notebook should be saved
+        site_url (str): URL of the site for metadata
+
+    Raises:
+        ValueError: If the file type is unsupported
+    """
+    if src_path.suffix == ".qmd":
+        # Use absolute paths to avoid issues when changin working directories
+        with temporary_softlink(src_path.absolute(), src_path.with_name(f"_{src_path.name}").absolute()) as link_path:
+            subprocess.run(
+                ["quarto", "render", str(link_path), "--profile", "notebook", "--output", out_path.name, "--no-execute", "--to", "ipynb", "--metadata", f"site-url:{site_url}"], 
+                cwd=out_path.parent,
+                check=True,
+            )
+    elif src_path.suffix == ".ipynb":
+        shutil.copyfile(src_path, out_path)
+    else:
+        raise ValueError(f"Unsupported file type: {src_path.suffix}")
+
+def clean_notebook(notebook_path):
+    """Remove hidden content and outputs from a notebook_path ipynb file"""
+    notebook = nb.read(notebook_path, as_version = 4)
     
     for cell in notebook["cells"]:
-        # remove code content designed to be hidden
-        cell["source"] = re.sub(r"#---[\S\s]*?#---", "", cell["source"])
-        
-        # delete quarto metadata on individual cells
+        # Remove code content designed to be hidden
+        message = "# TODO" if cell["cell_type"] == "code" else "*[TODO]*"
+        cell["source"] = re.sub(r"#---[\S\s]*?#---", message, cell["source"])
+        cell.source = re.sub(r"::: {.hide .solution}[\S\s]*?:::", "*[your response here]*", cell.source)
+        cell.source = re.sub(r"::: {.solution .hide}[\S\s]*?:::", "*[your response here]*", cell.source)
+
+        # Delete quarto metadata on individual cells
         cell["source"] = re.sub(r"#\|.*\n", "", cell["source"])
-        
-        # delete YAML metadata in notebook header 
-        cell["source"] = re.sub(r"---[\S\s]*?---", "", cell["source"])
-        
-        # clear outputs
+
+        # Clear outputs
         cell["outputs"] = []
-        
-        # reset execution count
+
+        # Reset execution count
         cell["execution_count"] = None
-        
-    nb.write(notebook, out_path)
+
+    nb.write(notebook, notebook_path)
+
+def main():
+    load_dotenv("_environment")  # Load quarto environment variables
+
+    parser = argparse.ArgumentParser(
+        description="Create cleaned Jupyter notebooks from source .qmd and .ipynb files"
+    )
+    parser.add_argument(
+        "--output_dir", default= Path("docs/live-notebooks"), type=Path, help="Output directory for notebooks"
+    )
+    parser.add_argument(
+        "--site_url", default=os.environ.get("SITE_URL"), help="Site URL for notebook URLs"
+    )
+    args = parser.parse_args()
+
+    args.output_dir.mkdir(parents=True, exist_ok=True)
     
+    for src_path in Path("source").glob("*"):
+        if not src_path.name.startswith("_") and src_path.suffix in {".qmd", ".ipynb"}:
+            out_path = args.output_dir / src_path.with_suffix(".ipynb").name
+            create_notebook(src_path, out_path, args.site_url)
+            clean_notebook(out_path)
+
+if __name__ == "__main__":
+    main()
+
 
 
 
